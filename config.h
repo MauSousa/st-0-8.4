839593591ed3dba411d8f51983ea672ae73c2d32
Augusto Sousa <lagrange.tesla@gmail.com>
2022-03-21 20:34:38 -0600

    Add alpha patch

--- config.h
+++ config.h
@@ -5,9 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=17:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=20:antialias=true:autohint=true";
 static char *font2 = "JoyPixels:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
+float alpha = 0.4;
 
 /*
  * What program is execed by st depends of these precedence rules:
